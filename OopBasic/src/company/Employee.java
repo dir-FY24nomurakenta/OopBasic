795a710c7faef03f08c756e3bf730402cb416f24
dir-FY24nomurakenta <KentaNomura()@Dirbato-1346x>
2024-04-08 11:53:44 +0900

    Chapter5

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -2,13 +2,13 @@ package company;
 
 public class Employee {
     //フィールド
-    String name;
-    String department;
-    String position;
-    int employeeId;
+    protected final String name;
+    private final Department department;
+    private final String position;
+    private final int employeeId;
     
     //コンストラクター
-    public Employee(String name, String department, String position, int employeeId) {
+    public Employee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
         this.position = position;
@@ -27,4 +27,11 @@ public class Employee {
         report(1);
     
     }
+    
+    //会議に参加するメソッド
+    public void joinMeeting() {
+        department.meeting();
+        System.out.println("→上記の会議に参加します。部署 : " + department.getName() + "名前 : " + name);
+        
+        }
 }
--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -1,26 +1,28 @@
 package company;
 
-import javax.xml.stream.XMLReporter;
-
 public class ObjectSample {
 
     public static void main(String[] args) {
         // インスタンスの生成
-        var employee = new Employee("鈴木", "営業部", "課長", 100);
+        var department = new Department("営業部", "xx", 1000000);
+        var employee = new Employee("鈴木", department, "課長", 100);
         
         // インスタンスメソッドの呼び出し
         employee.report();
         employee.report(2);
+        employee.joinMeeting();
         
         //1行空ける
         System.out.println("");
         
         // インスタンスの作成
-        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+        var devDepartment = new Department("開発部", "yy", 0);
+        var engineer = new Employee("田中", devDepartment, "一般社員", 88);
         
         // インスタンスメソッドの呼び出し
         engineer.report();
-      
+        engineer.joinMeeting();
+        
          }
 
 }
