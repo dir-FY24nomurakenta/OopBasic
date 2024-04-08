9e4a39ae7d8b8812ef9bd5d702d8cba5068c2db9
dir-FY24nomurakenta <KentaNomura()@Dirbato-1346x>
2024-04-08 11:01:15 +0900

    Chapter4

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -18,7 +18,7 @@ public class Employee {
     
     //報告メソッド
     public void report(int times) {
-        System.out.println(times + "回目の報告をします。役職： " + position + " 名前： " + name);
+        System.out.println(times + "回目の報告をします。役職： " + position + "、名前： " + name);
         
     }
     
